a94d8cee6afbfc006707a6a6ba34b709d9bdbc6c
Dr-Electron <dr-electr0n@protonmail.com>
2022-09-27 22:15:04 +0200

    Remove docs version info

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -5,18 +5,11 @@ module.exports = {
         [
             '@docusaurus/plugin-content-docs',
             {
-                id: 'inx-indexer-develop',
+                id: 'inx-indexer',
                 path: path.resolve(__dirname, 'docs'),
                 routeBasePath: 'inx-indexer',
                 sidebarPath: path.resolve(__dirname, 'sidebars.js'),
                 editUrl: 'https://github.com/iotaledger/inx-indexer/edit/develop/documentation',
-                versions: {
-                    current: {
-                        label: 'Develop',
-                        path: 'develop',
-                        badge: true
-                    },
-                },
             }
         ],
     ],
