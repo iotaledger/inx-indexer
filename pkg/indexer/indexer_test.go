322ba4a4625ad29ee1b03798287dd10df5657dae
Alexander Sporn <github@alexsporn.de>
2023-10-19 12:50:53 +0200

    Added tests for Account and Delegation outputs

--- pkg/indexer/indexer_test.go
+++ pkg/indexer/indexer_test.go
@@ -2,11 +2,13 @@ package indexer_test
 
 import (
 	"context"
+	"crypto/ed25519"
 	"math"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
+	hive_ed25519 "github.com/iotaledger/hive.go/crypto/ed25519"
 	"github.com/iotaledger/hive.go/logger"
 	"github.com/iotaledger/hive.go/runtime/options"
 	"github.com/iotaledger/inx-indexer/pkg/database"
@@ -103,6 +105,22 @@ func (os *indexerOutputSet) requireNotFoundWithBasicFilters(filters ...options.O
 	require.NotEqual(os.ts.T, os.Outputs, os.ts.Indexer.BasicOutputsWithFilters(filters...).OutputIDs)
 }
 
+func (os *indexerOutputSet) requireFoundWithAccountFilters(filters ...options.Option[indexer.AccountFilterOptions]) {
+	require.Equal(os.ts.T, os.Outputs, os.ts.Indexer.AccountOutputsWithFilters(filters...).OutputIDs)
+}
+
+func (os *indexerOutputSet) requireNotFoundWithAccountFilters(filters ...options.Option[indexer.AccountFilterOptions]) {
+	require.NotEqual(os.ts.T, os.Outputs, os.ts.Indexer.AccountOutputsWithFilters(filters...).OutputIDs)
+}
+
+func (os *indexerOutputSet) requireFoundWithDelegationFilters(filters ...options.Option[indexer.DelegationFilterOptions]) {
+	require.Equal(os.ts.T, os.Outputs, os.ts.Indexer.DelegationsWithFilters(filters...).OutputIDs)
+}
+
+func (os *indexerOutputSet) requireNotFoundWithDelegationFilters(filters ...options.Option[indexer.DelegationFilterOptions]) {
+	require.NotEqual(os.ts.T, os.Outputs, os.ts.Indexer.DelegationsWithFilters(filters...).OutputIDs)
+}
+
 func TestIndexer_BasicOutput(t *testing.T) {
 	ts := newTestSuite(t)
 
@@ -145,7 +163,10 @@ func TestIndexer_BasicOutput(t *testing.T) {
 
 	outputSet := ts.AddOutput(output, iotago_tpkg.RandOutputID(0))
 	require.Equal(t, iotago.SlotIndex(1), ts.CurrentSlot())
+
 	outputSet.requireFoundWithBasicFilters()
+	outputSet.requireNotFoundWithAccountFilters()
+	outputSet.requireNotFoundWithDelegationFilters()
 
 	// Check if the output is indexed correctly
 	outputSet.requireFoundWithBasicFilters(indexer.BasicOutputHasNativeToken(false))
@@ -233,7 +254,10 @@ func TestIndexer_BasicOutput_NativeToken(t *testing.T) {
 
 	outputSet := ts.AddOutput(output, iotago_tpkg.RandOutputID(0))
 	require.Equal(t, iotago.SlotIndex(1), ts.CurrentSlot())
+
 	outputSet.requireFoundWithBasicFilters()
+	outputSet.requireNotFoundWithAccountFilters()
+	outputSet.requireNotFoundWithDelegationFilters()
 
 	// Check if the output is indexed correctly
 	outputSet.requireFoundWithBasicFilters(indexer.BasicOutputHasNativeToken(true))
@@ -241,4 +265,149 @@ func TestIndexer_BasicOutput_NativeToken(t *testing.T) {
 
 	outputSet.requireFoundWithBasicFilters(indexer.BasicOutputNativeToken(nativeTokenID))
 	outputSet.requireNotFoundWithBasicFilters(indexer.BasicOutputNativeToken(iotago_tpkg.RandNativeTokenID()))
+}
+
+func TestIndexer_AccountOutput(t *testing.T) {
+	ts := newTestSuite(t)
+
+	randomAddress := iotago_tpkg.RandEd25519Address()
+
+	accountAddress := iotago_tpkg.RandAccountAddress()
+	senderAddress := iotago_tpkg.RandEd25519Address()
+	issuerAddress := iotago_tpkg.RandEd25519Address()
+	stateControllerAddress := iotago_tpkg.RandEd25519Address()
+	governorAddress := iotago_tpkg.RandEd25519Address()
+
+	output := &iotago.AccountOutput{
+		Amount:         iotago.BaseToken(iotago_tpkg.RandUint64(uint64(iotago_tpkg.TestAPI.ProtocolParameters().TokenSupply()))),
+		Mana:           iotago.Mana(iotago_tpkg.RandUint64(math.MaxUint64)),
+		AccountID:      accountAddress.AccountID(),
+		StateIndex:     0,
+		StateMetadata:  nil,
+		FoundryCounter: 0,
+		Conditions: iotago.AccountOutputUnlockConditions{
+			&iotago.StateControllerAddressUnlockCondition{
+				Address: stateControllerAddress,
+			},
+			&iotago.GovernorAddressUnlockCondition{
+				Address: governorAddress,
+			},
+		},
+		Features: iotago.AccountOutputFeatures{
+			&iotago.SenderFeature{
+				Address: senderAddress,
+			},
+			&iotago.StakingFeature{
+				StakedAmount: 6598,
+				FixedCost:    0,
+				StartEpoch:   0,
+				EndEpoch:     0,
+			},
+			&iotago.BlockIssuerFeature{
+				BlockIssuerKeys: iotago.BlockIssuerKeys{
+					&iotago.Ed25519PublicKeyBlockIssuerKey{
+						PublicKey: hive_ed25519.PublicKey(iotago_tpkg.RandEd25519PrivateKey().Public().(ed25519.PublicKey)),
+					},
+				},
+				ExpirySlot: 0,
+			},
+		},
+		ImmutableFeatures: iotago.AccountOutputImmFeatures{
+			&iotago.IssuerFeature{
+				Address: issuerAddress,
+			},
+		},
+	}
+
+	outputSet := ts.AddOutput(output, iotago_tpkg.RandOutputID(0))
+	require.Equal(t, iotago.SlotIndex(1), ts.CurrentSlot())
+
+	outputSet.requireFoundWithAccountFilters()
+	outputSet.requireNotFoundWithBasicFilters()
+	outputSet.requireNotFoundWithDelegationFilters()
+
+	outputSet.requireFoundWithAccountFilters(indexer.AccountCreatedAfter(0))
+	outputSet.requireNotFoundWithAccountFilters(indexer.AccountCreatedAfter(1))
+
+	outputSet.requireNotFoundWithAccountFilters(indexer.AccountCreatedBefore(0))
+	outputSet.requireNotFoundWithAccountFilters(indexer.AccountCreatedBefore(1))
+	outputSet.requireFoundWithAccountFilters(indexer.AccountCreatedBefore(2))
+
+	outputSet.requireFoundWithAccountFilters(indexer.AccountStateController(stateControllerAddress))
+	outputSet.requireNotFoundWithAccountFilters(indexer.AccountStateController(governorAddress))
+
+	outputSet.requireFoundWithAccountFilters(indexer.AccountGovernor(governorAddress))
+	outputSet.requireNotFoundWithAccountFilters(indexer.AccountGovernor(stateControllerAddress))
+
+	outputSet.requireFoundWithAccountFilters(indexer.AccountSender(senderAddress))
+	outputSet.requireNotFoundWithAccountFilters(indexer.AccountSender(randomAddress))
+
+	outputSet.requireFoundWithAccountFilters(indexer.AccountIssuer(issuerAddress))
+	outputSet.requireNotFoundWithAccountFilters(indexer.AccountIssuer(randomAddress))
+
+	// Unlockable by the following addresses
+	for _, addr := range []iotago.Address{stateControllerAddress, governorAddress} {
+		outputSet.requireFoundWithAccountFilters(indexer.AccountUnlockableByAddress(addr))
+	}
+
+	// Not unlockable by the following addresses
+	for _, addr := range []iotago.Address{senderAddress, issuerAddress, accountAddress} {
+		outputSet.requireNotFoundWithAccountFilters(indexer.AccountUnlockableByAddress(addr))
+	}
+}
+
+func TestIndexer_DelegationOutput(t *testing.T) {
+	ts := newTestSuite(t)
+
+	randomAddress := iotago_tpkg.RandEd25519Address()
+	randomValidatorAddress := iotago_tpkg.RandAccountAddress()
+
+	address := iotago_tpkg.RandEd25519Address()
+	validatorAddress := iotago_tpkg.RandAccountAddress()
+
+	amount := iotago.BaseToken(iotago_tpkg.RandUint64(uint64(iotago_tpkg.TestAPI.ProtocolParameters().TokenSupply())))
+
+	output := &iotago.DelegationOutput{
+		Amount:           amount,
+		DelegatedAmount:  amount,
+		DelegationID:     iotago.DelegationID{},
+		ValidatorAddress: validatorAddress,
+		StartEpoch:       0,
+		EndEpoch:         0,
+		Conditions: iotago.DelegationOutputUnlockConditions{
+			&iotago.AddressUnlockCondition{
+				Address: address,
+			},
+		},
+	}
+
+	outputSet := ts.AddOutput(output, iotago_tpkg.RandOutputID(0))
+	require.Equal(t, iotago.SlotIndex(1), ts.CurrentSlot())
+
+	outputSet.requireFoundWithDelegationFilters()
+	outputSet.requireNotFoundWithAccountFilters()
+	outputSet.requireNotFoundWithBasicFilters()
+
+	outputSet.requireFoundWithDelegationFilters(indexer.DelegationCreatedAfter(0))
+	outputSet.requireNotFoundWithDelegationFilters(indexer.DelegationCreatedAfter(1))
+
+	outputSet.requireNotFoundWithDelegationFilters(indexer.DelegationCreatedBefore(0))
+	outputSet.requireNotFoundWithDelegationFilters(indexer.DelegationCreatedBefore(1))
+	outputSet.requireFoundWithDelegationFilters(indexer.DelegationCreatedBefore(2))
+
+	outputSet.requireFoundWithDelegationFilters(indexer.DelegationAddress(address))
+	outputSet.requireNotFoundWithDelegationFilters(indexer.DelegationAddress(randomAddress))
+
+	outputSet.requireFoundWithDelegationFilters(indexer.DelegationValidator(validatorAddress))
+	outputSet.requireNotFoundWithDelegationFilters(indexer.DelegationValidator(randomValidatorAddress))
+
+	// Unlockable by the following addresses
+	for _, addr := range []iotago.Address{address} {
+		outputSet.requireFoundWithAccountFilters(indexer.AccountUnlockableByAddress(addr))
+	}
+
+	// Not unlockable by the following addresses
+	for _, addr := range []iotago.Address{validatorAddress} {
+		outputSet.requireNotFoundWithAccountFilters(indexer.AccountUnlockableByAddress(addr))
+	}
 }
